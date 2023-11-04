f05b260cd6a54395a734a189ca69aeb2f24dbea0
Committe Code <committedcode.io@gmail.com>
2023-11-04 19:49:39 -0400

    Enable connection to database.

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = 5000;
 
@@ -6,6 +7,15 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+mongoose
+  .connect("mongodb://127.0.0.1/simple-flash/")
+  .then(() => {
+    console.log("Connected to MongoDB");
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Connection to MongoDB failed:", error);
+  });
