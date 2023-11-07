64af7e86e8d9bde4886503a75b8f2f1799c11606
Committe Code <committedcode.io@gmail.com>
2023-11-06 19:59:11 -0500

    Add cors.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const groupRoutes = require("./routes/groupRoutes");
 const flashcardRoutes = require("./routes/flashcardRoutes");
 const app = express();
 const port = 5000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", groupRoutes);
 app.use("/api", flashcardRoutes);
